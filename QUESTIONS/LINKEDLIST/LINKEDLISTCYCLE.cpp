b568935c60848b61396ff1be10879bdefef2c909
Manik Sharma <casanova270407@gmail.com>
2025-12-22 21:38:22 +0530

    easy part is kinda done

---
+++ QUESTIONS/LINKEDLIST/LINKEDLISTCYCLE.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head) return false;
+
+        ListNode* slow=head;
+        ListNode* fast=head->next;
+        while(fast&&fast->next)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+
+            if(slow==fast)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+int main()
+{
+//created by manik sharma
+
+return 0 ;
+}
