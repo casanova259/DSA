9c076f6a312d0deb216fd4077d5f1de106aec728
Manik Sharma <casanova270407@gmail.com>
2025-12-23 21:17:30 +0530

    completed two problems of binary tree

--- QUESTIONS/LINKEDLIST/MIDDLE_OF_LINKED_LIST.cpp
+++ QUESTIONS/LINKEDLIST/MIDDLE_OF_LINKED_LIST.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
 using namespace std;
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
+//  * Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
 public:
-    ListNode* middleNode(ListNode* head) {
-        ListNode* slow=head;
-        ListNode* fast=head;
-        while(slow!=NULL&&fast!=NULL)
+    ListNode *middleNode(ListNode *head)
+    {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (slow != NULL && fast != NULL)
         {
-            fast=fast->next;
-            if(fast!=NULL)
+            fast = fast->next;
+            if (fast != NULL)
             {
-                fast=fast->next;
-                slow=slow->next;
+                fast = fast->next;
+                slow = slow->next;
             }
         }
         return slow;
@@ -29,7 +31,7 @@ public:
 };
 int main()
 {
-//created by manik sharma
+    // created by manik sharma
 
-return 0 ;
+    return 0;
 }
