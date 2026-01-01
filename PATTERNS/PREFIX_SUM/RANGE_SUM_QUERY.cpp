029e91885743c6f83ae630959b4a62522deffc7d
Manik Sharma <casanova270407@gmail.com>
2026-01-01 13:06:17 +0530

    RANGE SUM SOLVED

---
+++ PATTERNS/PREFIX_SUM/RANGE_SUM_QUERY.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include<vector>
+#include<limits>
+#include<unordered_map>
+using namespace std;
+class NumArray {
+public:
+    vector<int> prefix;
+    NumArray(vector<int>& nums) {
+        prefix.resize(nums.size()+1);
+
+        //prefix hum yahi calculate krlege
+        for(int i =0;i<nums.size();i++){
+            prefix[i+1]=prefix[i]+nums[i];
+        }
+    }
+
+    int sumRange(int left, int right) {
+
+        
+            return prefix[right + 1] - prefix[left];
+
+        
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
+
+
+int main()
+{
+//created by manik sharma
+
+return 0 ;
+}
