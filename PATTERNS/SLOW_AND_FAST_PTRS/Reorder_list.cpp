e19b243679ed4d3ae0c0c3faf8443f980740abeb
Manik Sharma <casanova270407@gmail.com>
2026-01-13 13:59:50 +0530

    Need to revise Tommorow

---
+++ PATTERNS/SLOW_AND_FAST_PTRS/Reorder_list.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+    ListNode(int x, ListNode *ptr) : val(x), next(ptr) {}
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    void reorderList(ListNode *head)
+    {
+
+        // step 1 :find the middle of the linked list
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while (fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // now the slow points at the mid of the linked list
+
+        // STEP 2: we reverse the second half
+        // starting from the node after slow , we use a standard iterative
+        // reversal process to  reverse the ptrs
+        ListNode *second = slow->next;
+        slow->next = nullptr;
+        ListNode *node = NULL;
+
+        while (second)
+        {
+            ListNode *temp = second->next;
+            second->next = node;
+            node = second;
+            second = temp;
+        }
+
+        // STEP #:Merge The Two Halves
+        ListNode *first = head;
+        second = node;
+
+        while (second)
+        {
+            ListNode *temp1 = first->next;
+            ListNode *temp2 = second->next;
+
+            first->next = second;
+            second->next = temp1;
+
+            first = temp1;
+            second = temp2;
+        }
+    }
+};
+int main()
+{
+    // created by manik sharma
+
+    return 0;
+}
