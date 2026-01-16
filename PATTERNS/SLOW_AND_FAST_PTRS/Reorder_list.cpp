44ba7950b1c2fccb1cbe8b447d30575d82e29807
Manik Sharma <casanova270407@gmail.com>
2026-01-16 11:25:32 +0530

    revised Reorder List lagged alot of things

--- PATTERNS/SLOW_AND_FAST_PTRS/Reorder_list.cpp
+++ PATTERNS/SLOW_AND_FAST_PTRS/Reorder_list.cpp
@@ -76,3 +76,59 @@ int main()
 
     return 0;
 }
+
+
+
+/*
+// REVISION - 16 jan 26
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     void reorderList(ListNode* head) {
+//         ListNode* slow=head;
+//         ListNode* fast=head ;
+
+//         while(fast&&fast->next){
+//             slow=slow->next;
+//             fast=fast->next->next;
+//         }
+
+//         //now here the slow reaches the mid of the linked list
+//         ListNode* second=slow->next;
+//         slow->next=nullptr;
+//         //so we did this to make the new list seperately and merge them afterwards
+//         ListNode* node=NULL;
+
+//         while(second){
+//             //now the logic to reverse the list 
+//             ListNode* temp=second->next;
+//             //jo node h thats the head ofnew reversed linked list
+//             second->next=node;
+//             node=second;
+//             second=temp;
+//         }
+
+//         ListNode* first=head;
+//          second=node;
+
+//         while(second){
+//             ListNode* temp1=first->next;
+//             ListNode* temp2=second->next;
+
+//             first->next=second;
+//             second->next=temp1;
+
+//             first=temp1;
+//             second=temp2;
+//         }
+//     }
+// };*/
