8c4264c1a9608c052c4a5db7233c013c273dbb73
Manik Sharma <casanova270407@gmail.com>
2026-01-17 11:25:58 +0530

    Solved it easily basics were looking good gonna start another one at monday

--- PATTERNS/SLOW_AND_FAST_PTRS/SortList.cpp
+++ PATTERNS/SLOW_AND_FAST_PTRS/SortList.cpp
@@ -100,3 +100,69 @@ int main()
 
     return 0;
 }
+
+/*
+class Solution {
+public:
+    ListNode* getmiddle(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+
+    ListNode* merge(ListNode* left, ListNode* right) {
+
+        if (!left)
+            return right;
+        if (!right)
+            return left;
+
+        ListNode* ans = new ListNode(-1, NULL);
+        ListNode* mptr = ans;
+
+        while (left && right) {
+            if (left->val < right->val) {
+                mptr->next = left;
+                mptr = left;
+                left = left->next;
+            } else {
+                mptr->next = right;
+                mptr = right;
+                right = right->next;
+            }
+        }
+
+        if (left) {
+            mptr->next = left;
+        }
+        if (right) {
+            mptr->next = right;
+        }
+
+        return ans->next;
+    }
+    ListNode* sortList(ListNode* head) {
+
+        if (!head || !head->next)
+            return head;
+
+        ListNode* middle = getmiddle(head);
+        ListNode* left = head;
+        ListNode* right = middle->next;
+        middle->next = nullptr;
+
+        left = sortList(left);
+        right = sortList(right);
+
+        ListNode* mergedLL = merge(left, right);
+
+        return mergedLL;
+    }
+};
+*/
