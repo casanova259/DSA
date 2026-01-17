f7ba172aeb5bc983fc1c2088a858e42cb037207b
Manik Sharma <casanova270407@gmail.com>
2026-01-17 11:21:55 +0530

    revised it on 17 jan confused about the logic

--- PATTERNS/SLOW_AND_FAST_PTRS/ADD_NUmbers.cpp
+++ PATTERNS/SLOW_AND_FAST_PTRS/ADD_NUmbers.cpp
@@ -74,3 +74,40 @@ int main()
 
     return 0;
 }
+
+/*class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int total=0;
+        int carry=0;
+        ListNode* ans=new ListNode(-1);
+        ListNode* dummy=ans;
+
+        while(l1||l2||carry){
+
+            total=carry;
+
+            if(l1){
+                total+=l1->val;
+                l1=l1->next;
+            }
+
+            if(l2){
+                total+=l2->val;
+                l2=l2->next;
+            }
+
+
+            //now processing the total if total>10 so there will a carry
+            int num=total%10;//extracting the number
+            carry=total/10;
+            dummy->next=new ListNode(num);
+            dummy=dummy->next;
+        }
+
+        ListNode* result=ans->next;
+        delete ans;
+        return result;
+    }
+};
+*/
