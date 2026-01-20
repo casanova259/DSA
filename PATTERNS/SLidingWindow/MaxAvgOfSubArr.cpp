7bb41e4295bc23f1958ba6b3abedf723aa965fd9
Manik Sharma <casanova270407@gmail.com>
2026-01-20 14:08:29 +0530

    Did it again as of start

--- PATTERNS/SLidingWindow/MaxAvgOfSubArr.cpp
+++ PATTERNS/SLidingWindow/MaxAvgOfSubArr.cpp
@@ -38,6 +38,43 @@ public:
         return avg;
     }
 };
+
+
+/*20 Jan 2025*/
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        double currAvg = 0;
+        double maxAvg = -1e9;
+        int n = nums.size();
+        double sum = 0;
+        double K = k;
+
+        // calculating the initial sum of the window
+        for (int i = 0; i < K; i++) {
+            sum += nums[i];
+            // calculating the first sum then setting that sum as curr average
+        }
+
+        currAvg = sum / K;
+        // then checking if the current average is greater than the curr or not
+        maxAvg = max(currAvg, maxAvg);
+
+        // now adding the new element to the window and removing the old one
+        // from the window
+        for (int i = K; i < n; i++) {
+            int newElement = nums[i];
+            int oldElement = nums[i - k];
+
+            // now adding that elements to the sum
+            sum = sum + newElement - oldElement;
+            currAvg = sum / K;
+            maxAvg = max(currAvg, maxAvg);
+        }
+
+        return maxAvg;
+    }
+};
 int main()
 {
 //created by manik sharma
