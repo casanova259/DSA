411c1f649dc69af4172af4d91e5cdeeab98d9f38
Manik Sharma <casanova270407@gmail.com>
2026-01-21 12:13:02 +0530

    handed it easily cz of dry run

--- PATTERNS/SLidingWindow/FruitsIntoBasket.cpp
+++ PATTERNS/SLidingWindow/FruitsIntoBasket.cpp
@@ -35,6 +35,30 @@ public:
         return maxi;
     }
 };
+
+
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        unordered_map<int,int> mp;
+        int maxi=0;
+        int left=0;
+
+        for(int right=0;right<fruits.size();right++){
+            mp[fruits[right]]++;
+
+            while(mp.size()>2){
+                mp[fruits[left]]--;
+                if(mp[fruits[left]]==0){
+                    mp.erase(fruits[left]);
+                }
+                left++;
+            }
+            maxi=max(maxi,right-left+1);
+        }
+        return maxi;
+    }
+};
 int main()
 {
     // created by manik sharma
