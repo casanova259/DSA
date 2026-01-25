aa901341def21d8687479192637f36f8de71cdff
Manik Sharma <casanova270407@gmail.com>
2026-01-25 23:44:35 +0530

    did nothing its life let's build it again

--- PATTERNS/SLidingWindow/Sliding_Window_max.cpp
+++ PATTERNS/SLidingWindow/Sliding_Window_max.cpp
@@ -45,6 +45,37 @@ public:
 
     }
 };
+
+class Solution2 {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> ans;
+        deque<int> dq;
+
+        for (int i = 0; i < n; i++) {
+            /*Har element ko window mein add karte hue process karte hain.*/
+
+            // Remove indices outside the window
+            if (!dq.empty() && dq.front() <= i - k) {
+                dq.pop_front();
+            }
+
+            // Maintain decreasing order in deque
+            while (!dq.empty() && nums[dq.back()] <= nums[i]) {
+                dq.pop_back();
+            }
+
+            dq.push_back(i);
+
+            // Add maximum for the current window
+            if (i >= k - 1)
+                ans.push_back(nums[dq.front()]);
+        }
+
+        return ans;
+    }
+};
 int main()
 {
 //created by manik sharma
