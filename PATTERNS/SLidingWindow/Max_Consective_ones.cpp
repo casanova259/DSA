2db5ac0349ca0bbe5b13161b03dc2a6a16f72ff1
Manik Sharma <casanova270407@gmail.com>
2026-02-01 15:39:41 +0530

    Nothing Special just revised sliding window

--- PATTERNS/SLidingWindow/Max_Consective_ones.cpp
+++ PATTERNS/SLidingWindow/Max_Consective_ones.cpp
@@ -2,6 +2,58 @@
 #include <limits>
 #include <vector>
 using namespace std;
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class BRUTE {
+public:
+    // Function to find the longest subarray with at most k zero flips
+    int longestOnes(vector<int>& nums, int k) {
+
+        // Variable to store the maximum length of valid subarray
+        int maxLen = 0;
+
+        // Loop to pick every possible starting point
+        for (int i = 0; i < nums.size(); i++) {
+
+            // Counter for zeros in the current subarray
+            int zeros = 0;
+
+            // Loop to pick every possible ending point
+            for (int j = i; j < nums.size(); j++) {
+
+                // If current element is zero, increment zero count
+                if (nums[j] == 0) {
+                    zeros++;
+                }
+
+                // If number of zeros exceeds k, break this subarray
+                if (zeros > k) {
+                    break;
+                }
+
+                // Update max length if this subarray is valid
+                maxLen = max(maxLen, j - i + 1);
+            }
+        }
+
+        // Return the length of the longest valid subarray
+        return maxLen;
+    }
+};
+
+// Driver code
+int main() {
+    Solution sol;
+    vector<int> nums = {1,1,1,0,0,0,1,1,1,1,0};
+    int k = 2;
+
+    // Output the result
+    cout << sol.longestOnes(nums, k) << endl;
+    return 0;
+}
+
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
