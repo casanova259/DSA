a12bc85d8b22f39903a888b8b768bd8309804970
Manik Sharma <casanova270407@gmail.com>
2026-02-11 23:28:11 +0530

    Revised Combination Sum II

--- PATTERNS/Recursion/Combination_Sum_II.cpp
+++ PATTERNS/Recursion/Combination_Sum_II.cpp
@@ -35,6 +35,47 @@ public:
     }
 };
 
+class SOL2 {
+public:
+    void solve(int index, int target, vector<int>& candidates,
+               vector<vector<int>>& ans, vector<int> ds) {
+
+        if (target == 0) {
+            ans.push_back(ds);
+            return;
+        }
+
+        //first thing the elements must be unique so
+        for(int i=index;i<candidates.size();i++)
+        {
+            //if both the elements are same we continue the iteration
+            //to Aviod the repeating combinations
+            if(i>index&&candidates[i]==candidates[i-1]) continue;
+
+            //now if the arr element is greater than the target so we break
+            if(candidates[i]>target) break;
+
+            //include the curr element
+            ds.push_back(candidates[i]);
+
+            //make the recursive call
+            solve(i+1,target-candidates[i],candidates,ans,ds);
+
+            //pop back the element
+            ds.pop_back();
+
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        // Sorting The Array to handle Duplicates
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> ans;
+        vector<int> ds;
+        solve(0, target, candidates, ans, ds);
+        return ans;
+    }
+};
+
 int main()
 {
 //created by manik sharma
