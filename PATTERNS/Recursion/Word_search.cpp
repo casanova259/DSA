b4f7f725f6cf50c4260f19c8d9d08e57ed6fe28c
Manik Sharma <casanova270407@gmail.com>
2026-02-17 23:10:05 +0530

    Searched The Word

---
+++ PATTERNS/Recursion/Word_search.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    bool dfs(vector<vector<char>> &board, string word, int i, int j, int idx)
+    {
+        if (idx == word.size())
+        {
+            return true;
+        }
+
+        // check boundaries and char match
+        if (i < 0 || j < 0 || i >= board.size() || j >= board[0].size() ||
+            board[i][j] != word[idx])
+        {
+            return false;
+        }
+        // Store current character and mark as visited
+        char temp = board[i][j];
+        board[i][j] = '#';
+
+        bool found = dfs(board, word, i + 1, j, idx + 1) ||
+                     dfs(board, word, i, j + 1, idx + 1) ||
+                     dfs(board, word, i, j - 1, idx + 1) ||
+                     dfs(board, word, i - 1, j, idx + 1);
+
+        // Restore the character (backtracking)
+        board[i][j] = temp;
+
+        return found;
+    }
+    bool exist(vector<vector<char>> &board, string word)
+    {
+
+        int rows = board.size();
+        int cols = board[0].size();
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if (dfs(board, word, i, j, 0))
+                {
+                    return true;
+                }
+            }
+        }
+
+        // if no path is found then we return false
+        return false;
+    }
+};
+int main()
+{
+    // created by manik sharma
+
+    return 0;
+}
