0cfa15be2c5290ea99cd03e660b53be72e7118de
Manik Sharma <casanova270407@gmail.com>
2026-02-19 00:51:58 +0530

    HEre We Go Again

--- PATTERNS/Recursion/N_Queens.cpp
+++ PATTERNS/Recursion/N_Queens.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    bool isSafe(int row, int col, vector<vector<char>>& board, int n) {
+    bool isSafe(int row, int col, vector<vector<char>> &board, int n)
+    {
         // check all the cols to the left in the same row
-        for (int j = 0; j < col; j++) {
+        for (int j = 0; j < col; j++)
+        {
             if (board[row][j] == 'Q')
                 return false;
         }
 
         // check for the upper left diagonal
-        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
+        {
             if (board[i][j] == 'Q')
                 return false;
         }
 
         // check for the lower left diagonal
-        for (int i = row, j = col; i < n && j >= 0; i++, j--) {
+        for (int i = row, j = col; i < n && j >= 0; i++, j--)
+        {
             if (board[i][j] == 'Q')
                 return false;
         }
@@ -26,13 +31,16 @@ public:
         return true;
     }
 
-    void solve(int col, vector<vector<char>>& board,
-               vector<vector<string>>& ans, int n) {
+    void solve(int col, vector<vector<char>> &board,
+               vector<vector<string>> &ans, int n)
+    {
         // if All cols are filled add curr board to the ans
-        if (col == n) {
+        if (col == n)
+        {
             vector<string> temp;
             // temp vecctor to store the solution
-            for (int i = 0; i < n; i++) {
+            for (int i = 0; i < n; i++)
+            {
                 // this converts all the rows of the vector into strigns and
                 // push them to the temp after that temp can be pushed in ans
                 string row(board[i].begin(), board[i].end());
@@ -44,21 +52,22 @@ public:
         }
 
         // Try Placing queen in all rows of the cols
-        for (int row = 0; row < n; row++) {
-            //place the queen to the postn if it is safe
-            if(isSafe(row,col,board,n))
+        for (int row = 0; row < n; row++)
+        {
+            // place the queen to the postn if it is safe
+            if (isSafe(row, col, board, n))
             {
-                //now place the queen to the board
-                board[row][col]='Q';
-                //recursive call for the next column
-                solve(col+1,board,ans,n);
-                //Backtrrack to explore all possible situations
-                board[row][col]='.';
-
+                // now place the queen to the board
+                board[row][col] = 'Q';
+                // recursive call for the next column
+                solve(col + 1, board, ans, n);
+                // Backtrrack to explore all possible situations
+                board[row][col] = '.';
             }
         }
     }
-    vector<vector<string>> solveNQueens(int n) {
+    vector<vector<string>> solveNQueens(int n)
+    {
         vector<vector<string>> ans;
 
         vector<vector<char>> board(n, vector<char>(n, '.'));
@@ -71,7 +80,7 @@ public:
 };
 int main()
 {
-//created by manik sharma
+    // created by manik sharma
 
-return 0 ;
+    return 0;
 }
