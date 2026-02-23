c0844bfca52c542cb76c70b7bc687e3c9ec5e7ae
Manik Sharma <casanova270407@gmail.com>
2026-02-23 22:55:26 +0530

    Lil differnent

--- PATTERNS/PREFIX_SUM/SubArray_SUM_EQ_K.cpp
+++ PATTERNS/PREFIX_SUM/SubArray_SUM_EQ_K.cpp
@@ -31,6 +31,37 @@ public:
         return count;   
     }
 };
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        //create a hashmap
+        unordered_map<int,int> mp;
+        //pehle ek condn lgagyege
+        mp[0]=1;
+        int count=0;
+        int prefixSum=0;
+
+        for(int num:nums)
+        {
+            
+            //update the prefix sum
+            prefixSum+=num;
+
+            if(mp.find(prefixSum-k)!=mp.end())
+            {
+                count+=mp[prefixSum-k];
+            }
+
+            //if i dont find the sum in the map we store it for further 
+            //processing
+             mp[prefixSum]++;
+        }     
+
+        return count;   
+    }
+};
+
+
 int main()
 {
 //created by manik sharma
