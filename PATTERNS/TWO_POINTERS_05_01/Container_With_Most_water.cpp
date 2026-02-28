2d5c93cbcc278b4caf9ddf3e925bdf6c56ec1690
Manik Sharma <casanova270407@gmail.com>
2026-02-28 23:30:33 +0530

    Did  it

--- PATTERNS/TWO_POINTERS_05_01/Container_With_Most_water.cpp
+++ PATTERNS/TWO_POINTERS_05_01/Container_With_Most_water.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <vector>
-#include<limits>
+#include <limits>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    int maxArea(vector<int>& height) {
+    int maxArea(vector<int> &height)
+    {
         // first we create two pointers one as left and other one as right
         int maxArea = 0;
         int left = 0;
         int right = height.size() - 1;
 
-        while (left < right) {
+        while (left < right)
+        {
             // now we would have to find the width which is right-left
             int width = right - left;
             // now the height will be the min of both cz if we take the bigger
@@ -25,18 +28,66 @@ public:
             maxArea = max(maxArea, width * hieght);
 
             // now when we have to move the pointers
-            if (height[left] < height[right]) {
+            if (height[left] < height[right])
+            {
                 // why we move the left ptr so that we can find the next
-                //bigger height which helps us to build the next conatiner whose
-                //area is larger
+                // bigger height which helps us to build the next conatiner whose
+                // area is larger
                 left++;
             }
 
-            else {
-                right--; 
+            else
+            {
+                right--;
                 // so movwe the right
                 // inwards we r doing it so that we can find the nexct greater height
-                //whcih can help us to calculate or make a bigger container
+                // whcih can help us to calculate or make a bigger container
+            }
+        }
+
+        return maxArea;
+    }
+};
+class Solution
+{
+public:
+    int maxArea(vector<int> &height)
+    {
+        // first we create two pointers one as left and other one as right
+        int maxArea = 0;
+        int left = 0;
+        int right = height.size() - 1;
+
+        while (left < right)
+        {
+            // now we would have to find the width which is right-left
+            int width = right - left;
+            // now the height will be the min of both cz if we take the bigger
+            // one the container must overflow
+            int hieght = min(height[left], height[right]);
+
+            // but we have to do it for the whole array so we make a condition
+            // left<=right
+
+            // now after getting the hieght and the width
+            // we caluclate the maxarea each time
+            maxArea = max(maxArea, width * hieght);
+
+            // now when we have to move the pointers
+            if (height[left] < height[right])
+            {
+                // why we move the left ptr so that we can find the next
+                // bigger height which helps us to build the next conatiner whose
+                // area is larger
+                left++;
+            }
+
+            else
+            {
+                right--;
+                // so movwe the right
+                // inwards we r doing it so that we can find the nexct greater height
+                // whcih can help us to calculate or make a bigger container
             }
         }
 
@@ -45,7 +96,7 @@ public:
 };
 int main()
 {
-//created by manik sharma
+    // created by manik sharma
 
-return 0 ;
+    return 0;
 }
