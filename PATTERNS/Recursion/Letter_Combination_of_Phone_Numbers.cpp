32b2717c2d8561cec140533a19a7e233b8c1c727
Manik Sharma <casanova270407@gmail.com>
2026-03-05 21:50:45 +0530

    Did it as it is

--- PATTERNS/Recursion/Letter_Combination_of_Phone_Numbers.cpp
+++ PATTERNS/Recursion/Letter_Combination_of_Phone_Numbers.cpp
@@ -52,6 +52,56 @@ public:
         return ans;
     }
 };
+//5 March 2026
+class Solution {
+public:
+    void solve(vector<string>& ans, int index, string digits, string output,
+               vector<string> mapping) {
+        
+        if(index==digits.size())
+        {
+            //Add The Current Combination to the answer
+            ans.push_back(output);
+            return;
+        }   
+
+        //convert the current Character to an integer
+        int digit=digits[index]-'0';
+        string value=mapping[digit];
+        //loop through the corressponding charcter
+        for(int i=0;i<value.length();i++)
+        {
+            char ch=value[i];
+            //adding the character to output
+            output.push_back(ch);
+            //making the recursive call
+            solve(ans,index+1,digits,output,mapping);
+            //backtracking
+            output.pop_back();
+        }     
+    }
+    vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        if (digits.length() == 0)
+            return ans;
+        int index = 0;
+        string output = "";
+        vector<string> mapping(10);
+        mapping[2] = "abc";
+        mapping[3] = "def";
+        mapping[4] = "ghi";
+        mapping[5] = "jkl";
+        mapping[6] = "mno";
+        mapping[7] = "pqrs";
+        mapping[8] = "tuv";
+        mapping[9] = "wxyz";
+        
+
+        solve(ans, 0, digits, output, mapping);
+
+        return ans;
+    }
+};
 int main()
 {
     // created by manik sharma
