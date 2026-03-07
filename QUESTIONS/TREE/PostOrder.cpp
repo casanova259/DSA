81295e2ec846bbe96c7ce50b2ee12db4b6e95d8c
Manik Sharma <casanova270407@gmail.com>
2026-03-07 22:44:41 +0530

    Now The PostOrder

---
+++ QUESTIONS/TREE/PostOrder.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * 
+ */
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+class Solution {
+public: 
+    void solve(TreeNode* root,vector<int>&ans)
+    {
+        if(root==NULL)
+            return ;
+        
+
+        solve(root->left,ans);
+        solve(root->right,ans);
+
+        ans.push_back(root->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        solve(root,ans);
+        return ans;
+    }
+};
+
+int main()
+{
+//created by manik sharma
+
+return 0 ;
+}
