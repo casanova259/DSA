fbdc0b255df662376ff9cf216038e8f239304420
Manik Sharma <casanova270407@gmail.com>
2026-03-15 14:52:25 +0530

    Bottom one is done

---
+++ QUESTIONS/TREE/Bottom_View_Of_BT.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <queue>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+class Solution
+{
+public:
+    vector<int> topView(Node *root)
+    {
+        vector<int> ans;
+
+        if (root == NULL)
+            return ans;
+
+        map<int, int> mpp;
+
+        queue<pair<Node *, int>> q;
+        q.push({root, 0});
+
+        while (!q.empty())
+        {
+            auto it = q.front();
+            q.pop();
+
+            Node *node = it.first;
+            int line = it.second;
+
+            
+                mpp[line] = node->data;
+            
+
+            if (node->left)
+                q.push({node->left, line - 1});
+
+            if (node->right)
+                q.push({node->right, line + 1});
+        }
+
+        for (auto it : mpp)
+        {
+            ans.push_back(it.second);
+        }
+
+        return ans;
+    }
+};
+int main()
+{
+    // created by manik sharma
+
+    return 0;
+}
