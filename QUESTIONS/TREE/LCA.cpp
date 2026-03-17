125d14eed8e75436aca9fb62c66214c67189b368
Manik Sharma <casanova270407@gmail.com>
2026-03-17 22:37:45 +0530

    LCA IMP ONE

---
+++ QUESTIONS/TREE/LCA.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        if(root==NULL||root==p||root==q) return root;
+
+        TreeNode* left=lowestCommonAncestor(root->left,p,q);
+        TreeNode* right=lowestCommonAncestor(root->right,p,q);
+
+        if(left&&right) return root;
+
+        return (left!=NULL) ? left:right;
+    }
+};
+int main()
+{
+    // created by manik sharma
+
+    return 0;
+}
