ca67e9ba74610dd34e3ef34fd4a1d01b576a18a3
Manik Sharma <casanova270407@gmail.com>
2026-03-21 11:02:50 +0530

    Did the Bf for it

---
+++ QUESTIONS/TREE/Count_COIMPLETE_TREE_NODES.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    void inorder(TreeNode *root, int &count)
+    {
+        if (root == NULL)
+            return;
+
+        count++;
+
+        inorder(root->left, count);
+        inorder(root->right, count);
+    }
+    int countNodes(TreeNode *root)
+    {
+        if (root == NULL)
+            return 0;
+
+        int count = 0;
+        inorder(root, count);
+
+        return count;
+    }
+};
+int main()
+{
+    // created by manik sharma
+
+    return 0;
+}
