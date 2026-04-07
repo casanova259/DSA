52da332630878a5a582aac2ed3126cdc7afeab38
Manik Sharma <casanova270407@gmail.com>
2026-04-08 00:08:22 +0530

    prefix  sum is revised

---
+++ PATTERNS/PREFIX_SUM/LONGEST_SUBARRAY_SUM_EQUALS_K.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> 
+int getLongestSubarray(vector<int>& nums, int k){
+    // Write your code here
+
+    int len=0;
+    int pf=0;
+    unordered_map<int,int>mp;
+    mp[0]=-1;
+
+    for(int i=0;i<nums.size();i++)
+    {
+        pf+=nums[i];
+
+        if(mp.find(pf-k)!=mp.end())
+        {
+            len=max(len,i-mp[pf-k]);
+        }
+        
+        // store only first occurrence
+        if(mp.find(pf) == mp.end())
+        {
+            mp[pf] = i;
+        }
+    }
+
+    return len;
+}
