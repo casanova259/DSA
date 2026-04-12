2a893b2a4e4c814bd44848af1dd46169d6f6a85c
Manik Sharma <casanova270407@gmail.com>
2026-04-12 22:43:43 +0530

    without any lib fxn

--- PATTERNS/TWO_POINTERS_05_01/VALID_PALINDROME.cpp
+++ PATTERNS/TWO_POINTERS_05_01/VALID_PALINDROME.cpp
@@ -25,6 +25,53 @@ public:
         return true;
     }
 };
+
+class Solution {
+public:
+    bool isalphanum(char ch)
+    {
+        if(
+            (ch>='a'&&ch<='z')||
+            (ch>='A'&&ch<='Z')||
+            (ch>='0'&&ch<='9')
+        )
+        {
+            return true;
+        }
+
+        return false;
+    }
+    char tolowerCase(char ch)
+    {
+        if(ch>='A'&&ch<='Z')
+        {
+            return ch-'A'+'a';
+        }
+
+        return ch;
+    }
+    bool isPalindrome(string s) {
+        int left=0;
+        int right=s.length()-1;
+
+        while(left<right)
+        {
+            while(left<right&&!isalphanum(s[left])) left++;
+
+            while(left<right&&!isalphanum(s[right])) right--;
+
+            if(tolowerCase(s[left])!=tolowerCase(s[right]))
+            {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
 int main()
 {
 //created by manik sharma
