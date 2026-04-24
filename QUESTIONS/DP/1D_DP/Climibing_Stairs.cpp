a9c204cb6d6f86d8ce36ff9334b6f8bb0ae5792d
Manik Sharma <casanova270407@gmail.com>
2026-04-24 21:48:43 +0530

    Started Dp Patterns

---
+++ QUESTIONS/DP/1D_DP/Climibing_Stairs.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    int climbStairs(int n)
+    {
+        vector<int> dp(n + 1, 0);
+
+        // now just had to initialise them
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+};
+int main()
+{
+    // created by manik sharma
+
+    return 0;
+}
