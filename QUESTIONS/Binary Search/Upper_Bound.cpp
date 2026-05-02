f37517a970cab964b66f6e3b95af0b6182317f33
Manik Sharma <casanova270407@gmail.com>
2026-05-02 23:18:42 +0530

    Same as before

---
+++ QUESTIONS/Binary Search/Upper_Bound.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int upperBound(vector<int> &arr, int x, int n)
+{
+    // Write your code here.
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] > x)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+
+            low = mid + 1;
+        }
+    }
+
+    return ans;
+}
+int main()
+{
+    // created by manik sharma
+
+    return 0;
+}
