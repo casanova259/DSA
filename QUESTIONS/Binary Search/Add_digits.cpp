c12ebbbf51e0bb85d9c601ff22aa140aee7318c8
Manik Sharma <casanova270407@gmail.com>
2026-05-04 22:07:35 +0530

    an easy one

---
+++ QUESTIONS/Binary Search/Add_digits.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int addDigits(int num) {
+        return 1+(num-1)%9;
+    }
+};
+int main()
+{
+//created by manik sharma
+
+return 0 ;
+}
