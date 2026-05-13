9d66210bc81f0b1ad3ae4585c20bf229696578ae
Manik Sharma <casanova270407@gmail.com>
2026-05-13 23:52:24 +0530

    Did one more

--- QUESTIONS/DP/PARTITON_EQUAL_SUMS.cpp
+++ QUESTIONS/DP/PARTITON_EQUAL_SUMS.cpp
@@ -1,64 +1,125 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-
-    bool solve(vector<int>&nums,int n, int sum)
+    bool solve(vector<int> &nums, int n, int sum)
     {
-        //make a dp table
-        vector<vector<bool>>t(n+1,vector<bool>(sum+1,false));
+        // make a dp table
+        vector<vector<bool>> t(n + 1, vector<bool>(sum + 1, false));
 
-        //initialisation krenge
-        for(int i=0;i<=n;i++)
+        // initialisation krenge
+        for (int i = 0; i <= n; i++)
         {
-            for(int j=0;j<=n;j++)
+            for (int j = 0; j <= n; j++)
             {
-                if(j==0) t[i][0]=true;
+                if (j == 0)
+                    t[i][0] = true;
             }
         }
 
-        //now we gonna build the aanswer
-        for(int i =1;i<=n;i++)
+        // now we gonna build the aanswer
+        for (int i = 1; i <= n; i++)
         {
-            for(int j=1;j<=sum;j++)
+            for (int j = 1; j <= sum; j++)
             {
-                if(nums[i-1]<=j)
+                if (nums[i - 1] <= j)
                 {
-                    t[i][j]=t[i-1][j-nums[i-1]]||t[i-1][j];
+                    t[i][j] = t[i - 1][j - nums[i - 1]] || t[i - 1][j];
                 }
                 else
                 {
-                    t[i][j]=t[i-1][j];
+                    t[i][j] = t[i - 1][j];
                 }
             }
         }
 
         return t[n][sum];
     }
-    bool canPartition(vector<int>& nums) {
-        int n =nums.size();
+    bool canPartition(vector<int> &nums)
+    {
+        int n = nums.size();
 
-        //calculating thr sum of the array nums
-        int sum=0;
-        for(int i=0;i<n;i++)
+        // calculating thr sum of the array nums
+        int sum = 0;
+        for (int i = 0; i < n; i++)
         {
-            sum+=nums[i];
+            sum += nums[i];
         }
 
-        if(sum&1)
+        if (sum & 1)
         {
             return false;
         }
         else
         {
-            return solve(nums,n,sum/2);
+            return solve(nums, n, sum / 2);
+        }
+    }
+};
+
+// revised it
+class Solution
+{
+public:
+    bool solve(vector<int> &nums, int n, int sum)
+    {
+        // make a dp table
+        vector<vector<bool>> t(n + 1, vector<bool>(sum + 1, false));
+
+        // initialisation krenge
+        for (int i = 0; i <= n; i++)
+        {
+            for (int j = 0; j <= n; j++)
+            {
+                if (j == 0)
+                    t[i][0] = true;
+            }
+        }
+
+        // now we gonna build the aanswer
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= sum; j++)
+            {
+                if (nums[i - 1] <= j)
+                {
+                    t[i][j] = t[i - 1][j - nums[i - 1]] || t[i - 1][j];
+                }
+                else
+                {
+                    t[i][j] = t[i - 1][j];
+                }
+            }
+        }
+
+        return t[n][sum];
+    }
+    bool canPartition(vector<int> &nums)
+    {
+        int n = nums.size();
+
+        // calculating thr sum of the array nums
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += nums[i];
+        }
+
+        if (sum & 1)
+        {
+            return false;
+        }
+        else
+        {
+            return solve(nums, n, sum / 2);
         }
     }
 };
 int main()
 {
-//created by manik sharma
+    // created by manik sharma
 
-return 0 ;
+    return 0;
 }
