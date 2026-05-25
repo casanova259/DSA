465f066f843c43518bdd9195ea11a57e2daa6c01
Manik Sharma <casanova270407@gmail.com>
2026-05-25 23:21:00 +0530

    did it understand it

---
+++ PATTERNS/BIT_MNUPLAtion/Subsets.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n=nums.size();
+
+        vector<vector<int>> ans;
+
+        int total=1<<n;
+
+        for(int mask=0;mask<total;mask++)
+        {
+            vector<int> subset;
+
+            for(int j=0;j<n;j++)
+            {
+                if(mask&(1<<j))
+                {
+                    //include nums[j]
+                    subset.push_back(nums[j]);
+                }
+            }
+
+            ans.push_back(subset);
+        }
+
+        return ans;
+    }
+};
+int main()
+{
+//created by manik sharma
+
+return 0 ;
+}
