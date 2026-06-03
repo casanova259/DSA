96072d4698028cf696e1f45ead55b5ffe28831dc
Manik Sharma <casanova270407@gmail.com>
2026-06-03 21:46:22 +0530

    First of third day

---
+++ QUESTIONS/ARRAY/RotateaMatrix.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include<algorithm>
+
+using namespace std;
+class Solution
+{
+public:
+    void rotate(vector<vector<int>> &matrix)
+    {
+
+        int n = matrix.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        // reverse each row then
+        for (int i = 0; i < n; i++)
+        {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
+int main()
+{
+    // created by manik sharma
+
+    return 0;
+}
