04cdc549462f287b25c9149ac47d4644c14ddc3c
Manik Sharma <casanova270407@gmail.com>
2026-06-08 23:03:46 +0530

    2nd of the day8

---
+++ QUESTIONS/ARRAY/LS_WITH_SUM_K.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int longestSubarrayWithSumK(vector<int> a, long long k)
+{
+    // Write your code here
+    int left = 0;
+    int right = 0;
+    long long sum = a[0];
+    int maxLen = 0;
+    int n = a.size();
+
+    while (right < n)
+    {
+        while (left <= right && sum > k)
+        {
+            sum -= a[left];
+            left++;
+        }
+        if (sum == k)
+        {
+            maxLen = max(maxLen, right - left + 1);
+        }
+
+        right++;
+        if (right < n)
+        {
+            sum += a[right];
+        }
+    }
+
+    return maxLen;
+}
+int main()
+{
+    // created by manik sharma
+
+    return 0;
+}
